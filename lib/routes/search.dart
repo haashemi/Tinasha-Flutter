0e828b5b9a6c40d3106e5200732c5d4860ddc7df
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-22 01:55:54 +0330

    feat: make search screen almost identical to RN version

--- lib/routes/search.dart
+++ lib/routes/search.dart
@@ -13,27 +13,37 @@ class _SearchRouteState extends State<SearchRoute> {
 
   @override
   Widget build(BuildContext context) {
+    final topPadding = MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: SearchBar(
-          autoFocus: true,
-          leading: BackButton(onPressed: () => context.pop()),
-          hintText: 'Search...',
-          trailing: <Widget>[
-            IconButton(
-              onPressed: () => setState(() {
-                searchMode = searchMode == 'anime' ? 'manga' : 'anime';
-              }),
-              icon: Icon(searchMode == 'anime'
-                  ? Icons.live_tv
-                  : Icons.menu_book_rounded,),
-            ),
-          ],
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(10, topPadding + 5, 10, 0),
+          child: SearchBar(
+            autoFocus: true,
+            leading: BackButton(onPressed: () => context.pop()),
+            hintText: 'Search...',
+            trailing: <Widget>[
+              IconButton(
+                onPressed: () => setState(() {
+                  searchMode = searchMode == 'anime' ? 'manga' : 'anime';
+                }),
+                icon: Icon(
+                  searchMode == 'anime'
+                      ? Icons.live_tv
+                      : Icons.menu_book_rounded,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      body: const Column(
-        
+      body: Center(
+        child: Text(
+          "Search for something",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
       ),
     );
   }
