ada63ba547adcd37cc94a056cf6488c586e9f94d
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-22 01:27:25 +0330

    feat: set system ui mode to edge-to-edge

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 import 'package:tinasha_v2/data/app_settings.dart';
 import 'package:tinasha_v2/router.dart';
@@ -8,11 +9,30 @@ class TinashaApp extends StatelessWidget {
 
   final AppSettings settings;
 
+  void _updateSystemUiOverlay(BuildContext context, ThemeMode themeMode) {
+    final brightness = MediaQuery.of(context).platformBrightness;
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarIconBrightness: switch (themeMode) {
+          ThemeMode.dark => Brightness.light,
+          ThemeMode.light => Brightness.dark,
+          ThemeMode.system =>
+            brightness == Brightness.dark ? Brightness.light : Brightness.dark
+        },
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return PreferenceBuilder<ThemeMode>(
       preference: settings.themeMode,
       builder: (context, themeMode) {
+        _updateSystemUiOverlay(context, themeMode);
+
         return MaterialApp.router(
           routerConfig: router,
           title: 'Tinasha',
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tinasha_v2/app.dart';
 import 'package:tinasha_v2/data/app_settings.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
   final settings = AppSettings();
   await settings.init();
