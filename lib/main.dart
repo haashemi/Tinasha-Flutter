2cce490ceb2d26c8d6776c66b1082066928b02ff
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-24 01:38:31 +0330

    feat: remove leading # from web-app using url_strategy

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tinasha_v2/app.dart';
 import 'package:tinasha_v2/data/app_settings.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +11,9 @@ Future<void> main() async {
 
   final settings = AppSettings();
   await settings.init();
+
+  // Remove the leading hash (#) from the URL of the web app
+  setPathUrlStrategy();
 
   runApp(TinashaApp(settings));
 }
