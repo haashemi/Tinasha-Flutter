e49bbfa186dc47b4d17928a86bfb0a0c7c4105d0
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-23 21:42:21 +0330

    feat: add dynamic colors

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
@@ -9,17 +10,28 @@ class TinashaApp extends StatelessWidget {
 
   final AppSettings settings;
 
+  static final _defaultLight = ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+  ).harmonized();
+
+  static final _defaultDark = ColorScheme.fromSeed(
+    seedColor: Colors.blue,
+    brightness: Brightness.dark,
+  ).harmonized();
+
   void _updateSystemUiOverlay(BuildContext context, ThemeMode themeMode) {
     final brightness = MediaQuery.of(context).platformBrightness;
 
+    final statusBarIconBrightness = switch (themeMode) {
+      ThemeMode.dark => Brightness.light,
+      ThemeMode.light => Brightness.dark,
+      ThemeMode.system =>
+        brightness == Brightness.dark ? Brightness.light : Brightness.dark
+    };
+
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-        statusBarIconBrightness: switch (themeMode) {
-          ThemeMode.dark => Brightness.light,
-          ThemeMode.light => Brightness.dark,
-          ThemeMode.system =>
-            brightness == Brightness.dark ? Brightness.light : Brightness.dark
-        },
+        statusBarIconBrightness: statusBarIconBrightness,
         statusBarColor: Colors.transparent,
         systemNavigationBarColor: Colors.transparent,
       ),
@@ -33,22 +45,25 @@ class TinashaApp extends StatelessWidget {
       builder: (context, themeMode) {
         _updateSystemUiOverlay(context, themeMode);
 
-        return MaterialApp.router(
-          routerConfig: router,
-          title: 'Tinasha',
-          theme: ThemeData(
-            colorSchemeSeed: Colors.blue,
-            useMaterial3: true,
-          ),
-          darkTheme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.blue,
-              brightness: Brightness.dark,
-            ),
-          ),
-          themeAnimationCurve: Curves.easeInOut,
-          themeMode: themeMode,
-          debugShowCheckedModeBanner: false,
+        return DynamicColorBuilder(
+          builder: (lightDynamic, darkDynamic) {
+            return MaterialApp.router(
+              routerConfig: router,
+              title: 'Tinasha',
+              theme: ThemeData(
+                useMaterial3: true,
+                colorScheme: lightDynamic?.harmonized() ?? _defaultLight,
+              ),
+              darkTheme: ThemeData(
+                useMaterial3: true,
+                brightness: Brightness.dark,
+                colorScheme: darkDynamic?.harmonized() ?? _defaultDark,
+              ),
+              themeAnimationCurve: Curves.easeInOut,
+              themeMode: themeMode,
+              debugShowCheckedModeBanner: false,
+            );
+          },
         );
       },
     );
