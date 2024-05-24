9565bd1f0b5a5dd57b632993ad88dc345ff7fb4b
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-24 18:52:08 +0330

    feat: enable go_router's URLReflectsImperativeAPIs option

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tinasha_v2/app.dart';
 import 'package:tinasha_v2/data/app_settings.dart';
 import 'package:url_strategy/url_strategy.dart';
@@ -9,11 +10,14 @@ Future<void> main() async {
 
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 
+  // Enable the imperative API to affect the browser URL bar.
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+
+  // Remove the leading hash (#) from the URL of the web app.
+  setPathUrlStrategy();
+
   final settings = AppSettings();
   await settings.init();
-
-  // Remove the leading hash (#) from the URL of the web app
-  setPathUrlStrategy();
 
   runApp(TinashaApp(settings));
 }
