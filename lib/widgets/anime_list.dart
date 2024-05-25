ce3f7fa5e167b83ddf66741e10983447a5c26bda
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-05-25 13:27:58 +0330

    feat: match AnimeList ui with the RN version

--- lib/widgets/anime_list.dart
+++ lib/widgets/anime_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 
 class AnimeList extends StatelessWidget {
   const AnimeList({super.key});
@@ -31,53 +30,76 @@ class AnimeList extends StatelessWidget {
               ),
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+                  padding: const EdgeInsets.fromLTRB(5, 20, 5, 10),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      const Text(
-                        'Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou Medereba Ii?',
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontSize: 16,
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou Medereba Ii?',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
                         ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          const Row(
-                            children: <Widget>[
-                              Icon(Icons.star, size: 20),
-                              Gap(5),
-                              Text('8.22'),
-                            ],
-                          ),
-                          const Row(
-                            children: <Widget>[
-                              Icon(Icons.play_circle_outlined, size: 20),
-                              Gap(5),
-                              Text(
-                                '5/12',
-                                style: TextStyle(fontSize: 17),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Theme(
+                              data: Theme.of(context).copyWith(
+                                canvasColor: Colors.transparent,
+                                chipTheme:
+                                    const ChipThemeData(shape: LinearBorder()),
                               ),
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              IconButton.outlined(
-                                onPressed: () {},
-                                icon: const Icon(Icons.add),
+                              child: Chip(
+                                label: Text(
+                                  "8.76",
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                ),
+                                avatar: const Icon(Icons.star),
                               ),
-                              const SizedBox(width: 10), // give it width
-                              IconButton.filled(
-                                onPressed: () {},
-                                icon: const Icon(Icons.edit_note),
+                            ),
+                            Theme(
+                              data: Theme.of(context).copyWith(
+                                canvasColor: Colors.transparent,
+                                chipTheme:
+                                    const ChipThemeData(shape: LinearBorder()),
                               ),
-                            ],
-                          ),
-                        ],
+                              child: Chip(
+                                label: Text(
+                                  "8/12",
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                ),
+                                avatar: const Icon(Icons.play_circle_outlined),
+                              ),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                IconButton.outlined(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.add),
+                                ),
+                                const SizedBox(width: 10), // give it width
+                                IconButton.filled(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.edit_note),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
